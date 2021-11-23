0f72bc9954a673fab024e96cc99a0566b2c91ac5
Elnura D <elnurab@gmail.com>
2021-11-23 16:18:21 +0800

    updates

--- home.js
+++ home.js
@@ -2,37 +2,95 @@
 
 const container = document.getElementById('container');
 const buttonWrap = document.getElementById('buttonWrap')
-//container.appendChild(element); - one container
-
-//function that creates a 16/16 grid of containers and appends them to container
 
 const button = document.createElement("button")
-button.innerHTML = "Clear"
+button.innerHTML = "Create"
 buttonWrap.appendChild(button).className = "button"
 
+//slider to set the number of boxes
+const slider = document.createElement("input");
+slider.setAttribute("type", "min", "max", "value", "class")
+slider.type = "range"
+slider.class = "slider"
+slider.min = '2';
+slider.max = '30';
+slider.value = '16'
+buttonWrap.appendChild(slider)
+console.log(slider.value)
 
-//onclick button sets the elements backgground do no color
+slider.oninput = ()=> {
+    sliderValue.innerHTML = `Value ${slider.value}X${slider.value}`
+    console.log(slider.value)
+}
 
-const createGrid = () =>{
+//number of grid col/row should change to slider.value
 
-    for (i = 0; i<16*16; i++){
+//display the number of boxes
+let sliderValue = document.createElement("p")
+sliderValue.innerHTML = `Value ${slider.value}X${slider.value}`
+console.log(sliderValue.innerHTML)
+buttonWrap.appendChild(sliderValue)
+
+
+
+
+//function to create grid
+const createGrid = (col, row) =>{
+
+    for (i = 0; i<col*row; i++){
          let element = document.createElement("div");
-        element.onmouseover = function(){
+         element.className = "gridDiv";
+         element.onmouseover = function(){
             this.style.backgroundColor = 'red'
         }
         
-        container.appendChild(element).className = "gridDiv"
-        console.log(i)
+        button.addEventListener('click', ()=>{
+            element.style.backgroundColor = '';
+          
+
+        })
+       
+        container.appendChild(element)
+       
     }
 
 
 }
 
-createGrid()
+
+// if(slider.value === '16'){
+//     createGrid(16,16)
+// }
+// else{
+//     createGrid(slider.value, slider.value)
+// }
+
+// const createGrid = (col, row) =>{
+
+//     for (i = 0; i<col*row; i++){
+//          let element = document.createElement("div");
+//         element.onmouseover = function(){
+//             this.style.backgroundColor = 'red'
+//         }
+//         button.addEventListener('click', ()=>{
+//             element.style.backgroundColor = '';
+
+//         })
+       
+//         container.appendChild(element).className = "gridDiv"
+       
+//     }
 
 
-button.addEventListener('click', createGrid)
+// }
 
-//when hover class changes to onhover and stays, i need a function
+
+
+
+createGrid(16, 16)
+
+
+
+
 
 
