a009632f6f41031a3fde0d710857d0900cc43db0
Elnura D <elnurab@gmail.com>
2021-11-26 15:24:46 +0800

    separated buttons

--- home.js
+++ home.js
@@ -2,11 +2,15 @@
 //select elements
 const container = document.getElementById('container');
 const buttonWrap = document.getElementById('buttonWrap')
+const clear = document.getElementById('clear')
 
 //create button element
 const button = document.createElement("button")
+const clearButton = document.createElement("button")
 button.innerHTML = "Create"
+clearButton.innerHTML = "Clear"
 buttonWrap.appendChild(button).className = "button"
+clear.appendChild(clearButton).className = "clearButton"
 
 // create slider element to change the number of boxes
 const slider = document.createElement("input");
@@ -31,7 +35,7 @@ sliderValue.innerHTML = `Value ${slider.value}X${slider.value}`
 console.log(sliderValue.innerHTML)
 buttonWrap.appendChild(sliderValue)
 
-
+// let containerGridStyle = document.createElement('style');
 
 //function to create a grid
 const createGrid = (col, row) =>{
@@ -44,7 +48,7 @@ const createGrid = (col, row) =>{
             console.log(i)
         }
         
-        button.addEventListener('click', ()=>{
+        clearButton.addEventListener('click', ()=>{
             element.style.backgroundColor = '';
             
 
@@ -65,10 +69,29 @@ const removeOldGrid =()=>{
     }
 }
 
+
+
+// container.style.display = "grid";
+// container.style.gridTemplateColumns = `repeat(auto-fill, 1fr)`;
+// container.style.gridTemplateRows =`repeat(auto-fill, 1fr)`;
+// container.style.justifyContent = 'center';
+// container.style.alignItems = 'center';
+
+container.style.display = "grid";
+container.style.gridTemplateColumns = `repeat(${parseInt(slider.value)},1fr)`;
+container.style.gridTemplateRows = `repeat(${parseInt(slider.value)}, 1fr)`;
+container.style.justifyContent = 'center';
+container.style.alignItems = 'center';
+
 //create new grid on click 
 button.onclick = ()=>{
     removeOldGrid()
     createGrid(parseInt(slider.value), parseInt(slider.value))
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = `repeat(${parseInt(slider.value)},1fr)`;
+    container.style.gridTemplateRows = `repeat(${parseInt(slider.value)}, 1fr)`;
+    container.style.justifyContent = 'center';
+    container.style.alignItems = 'center';
 
     
     
